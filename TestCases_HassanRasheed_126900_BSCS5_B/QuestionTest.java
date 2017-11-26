b8ae482e2bebb12112012cd560fed79ccd7e353f
Hassan Rasheed <hassan.rasheed8@gmail.com>
2017-11-26 18:16:04 +0500

    Add files via upload

---
+++ TestCases_HassanRasheed_126900_BSCS5_B/QuestionTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import Entity.Question;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ *
+ * @author hassa
+ */
+public class QuestionTest extends TestCase {
+    @Test
+    public void testQuestionsEquivalency() {
+        Question qa=new Question("MCQ","Earth is Flat","False");
+        Question qb=new Question("MCQ","Earth is Flat","False","Yes","True","Almost","False");
+        Question qc=new Question("True/False","Earth is Flat","False");
+        Question qd=new Question("True/False","Earth is Flat","True");
+        Question qe=new Question("Numeric","What is 2+7","9");
+        Question qf=new Question("Numeric","What is 2+5","7");
+        assertFalse(qa.equals(qb));
+        assertFalse(qc.equals(qd));
+        assertFalse(qe.equals(qf));
+    }
+    
+}
---
+++ TestCases_HassanRasheed_126900_BSCS5_B/UserTest.java
@@ -0,0 +1,23 @@
+import Entity.User;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ *
+ * @author hassa
+ */
+public class UserTest extends TestCase {
+@Test    
+public void testUsersEquality(){
+    User student=new User("Hassan","Rasheed","Student");
+    User instructor=new User("Hassan","Rasheed","Instructor");
+    User student1=new User("Hassan","Rasheed","Instructor");
+    User instructor1=new User("Hassan","Rasheed","Student");
+    User student2=new User("Hassan","Rasheed","Student");
+    User student3=new User("Hassan","Rasheed","Student",0);
+    assertFalse(student.equals(instructor));
+    assertFalse(student1.equals(instructor1));
+    assertFalse(student2.equals(student3));
+}
+    
+}
